62a2f0f2c858a96e06d7631caeab052003298a89
Kallst <santiago.castrom@autonoma.edu.co>
2026-09-08 19:49:02 -0500

    Revert "Feature/f02 teams domain"

--- Backend/src/context/teams/domain/exceptions/TeamMemberLimitExceededError.ts
+++
@@ -1,6 +0,0 @@
-export class TeamMemberLimitExceededError extends Error {
-  constructor(teamId: string, limit: number) {
-    super(`El equipo ${teamId} alcanzó el límite de ${limit} integrantes`);
-    this.name = 'TeamMemberLimitExceededError';
-  }
-}
--- Backend/src/context/teams/domain/exceptions/TeamNotFoundError.ts
+++
@@ -1,6 +0,0 @@
-export class TeamNotFoundError extends Error {
-  constructor(id: string) {
-    super(`Equipo con id ${id} no encontrado`);
-    this.name = 'TeamNotFoundError';
-  }
-}
